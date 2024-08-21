d4efc02b29032707d2be9e30c00d7d3dba959334
telkomdev-Damia <damiaralitsa@gmail.com>
2024-08-21 16:41:20 +0700

    feat: Update module imports and add new dependencies
    This commit updates the module imports in the notification modules to use the latest versions of the notif-lib-go modules. It also adds a new dependency on the "github.com/lib/pq" package. These changes ensure that the codebase is up to date with the latest features and bug fixes, and that the required dependencies are correctly specified.

--- notification/config/notif.go
+++ notification/config/notif.go
@@ -1,21 +1,27 @@
 package config
 
-type NotifConfig struct {
-	FabdCoreUrl   string `env:"FABD_CORE_URL" envDefault:"" json:"notif_fabd_core_url"`
-	EmailHost     string `env:"EMAIL_HOST" envDefault:"" json:"notif_email_host"`
-	EmailPort     string `env:"EMAIL_PORT" envDefault:"" json:"notif_email_port"`
-	EmailUserName string `env:"EMAIL_USERNAME" envDefault:"" json:"notif_email_username"`
-	EmailPassword string `env:"EMAIL_PASSWORD" envDefault:"" json:"notif_email_password"`
-	OCAWABASEURL  string `env:"OCA_WA_BASE_URL" envDefault:"" json:"notif_oca_wa_base_url"`
-	OCAWAToken    string `env:"OCA_WA_TOKEN" envDefault:"" json:"notif_oca_wa_token"`
-	BellType      string `env:"BELL_TYPE" envDefault:"" json:"notif_bell_type"`
-	BellHost      string `env:"BELL_HOST" envDefault:"" json:"notif_bell_host"`
-	BellPort      string `env:"BELL_PORT" envDefault:"" json:"notif_bell_port"`
-	BellUsername  string `env:"BELL_USERNAME" envDefault:"" json:"notif_bell_username"`
-	BellPassword  string `env:"BELL_PASSWORD" envDefault:"" json:"notif_bell_password"`
-	BellDatabase  string `env:"BELL_DATABASE" envDefault:"" json:"notif_bell_database"`
+type EmailConfig struct {
+	EmailHost     string `env:"EMAIL_HOST" envDefault:"" json:"notif_email_host" validate:"required"`
+	EmailPort     string `env:"EMAIL_PORT" envDefault:"" json:"notif_email_port" validate:"required"`
+	EmailUserName string `env:"EMAIL_USERNAME" envDefault:"" json:"notif_email_username" validate:"required"`
+	EmailPassword string `env:"EMAIL_PASSWORD" envDefault:"" json:"notif_email_password" validate:"required"`
 }
 
-func (c *NotifConfig) InitEnv() error {
+type OCAConfig struct {
+	OCAWABASEURL string `env:"OCA_WA_BASE_URL" envDefault:"" json:"notif_oca_wa_base_url" validate:"required"`
+	OCAWAToken   string `env:"OCA_WA_TOKEN" envDefault:"" json:"notif_oca_wa_token" validate:"required"`
+}
+
+type BellConfig struct {
+	FabdCoreUrl  string `env:"FABD_CORE_URL" envDefault:"" json:"notif_fabd_core_url" validate:"required"`
+	BellType     string `env:"BELL_TYPE" envDefault:"" json:"notif_bell_type" validate:"required"`
+	BellHost     string `env:"BELL_HOST" envDefault:"" json:"notif_bell_host" validate:"required"`
+	BellPort     string `env:"BELL_PORT" envDefault:"" json:"notif_bell_port" validate:"required"`
+	BellUsername string `env:"BELL_USERNAME" envDefault:"" json:"notif_bell_username" validate:"required"`
+	BellPassword string `env:"BELL_PASSWORD" envDefault:"" json:"notif_bell_password" validate:"required"`
+	BellDatabase string `env:"BELL_DATABASE" envDefault:"" json:"notif_bell_database" validate:"required"`
+}
+
+func InitEnv(c any) error {
 	return envLoader(c, OptionsEnv{DotEnv: true, Prefix: "NOTIF_"})
 }
